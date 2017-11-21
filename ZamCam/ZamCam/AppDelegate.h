3825662a0d128272d052f53796f1b9b5bd46730d
zamjaewook <zamcom@naver.com>
2017-11-21 21:18:42 +0900

    Add my default files
    Add my default files

---
+++ ZamCam/ZamCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZamCam
+//
+//  Created by zamcom on 2017. 11. 21..
+//  Copyright © 2017년 zamcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZamCam/ZamCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZamCam
+//
+//  Created by zamcom on 2017. 11. 21..
+//  Copyright © 2017년 zamcom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
